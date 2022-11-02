21205f5957f8bf47218e7e87df149c59279e871a
KazooTTT <wangyijin1999@qq.com>
2022-11-02 19:00:31 +0800

    feat: 优化接口

--- src/api/layout/controllers/layout.js
+++ src/api/layout/controllers/layout.js
@@ -1,9 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  * layout controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
+const { removeTime, removeAttrsAndId } = require("../../../utils/index.js");
 
-module.exports = createCoreController('api::layout.layout');
+module.exports = createCoreController("api::layout.layout", ({ strapi }) => ({
+  async find(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: "deep",
+    };
+    const { data } = await super.find(ctx);
+    // 取data[0]是因为只有一个layout
+    return removeAttrsAndId(removeTime(data[0]));
+  },
+}));
